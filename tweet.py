a4da86eb660e60d91fb689e6bd438fba622b2518
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-07-26 10:42:38 +0100

    Start writing bot

--- tweet.py
+++ tweet.py
@@ -1,16 +1,25 @@
 from twitter import *
-from random import choice
 from datetime import datetime
+from dayfinder import get_approximation_day
+import sys
 
-print(datetime.now())
+test = "test" in sys.argv
 
-import config
+if test:
+    print("Running in test mode. Tweets will not be sent")
 
-twitter = Twitter(
-    auth = OAuth(config.token, config.secret, config.consumer_key, config.consumer_secret))
+now = datetime.now()
 
-tweet = "Bello world!"
+approx_day = get_approximation_day(now.day, now.month)
 
-results = twitter.statuses.update(status=tweet)
-print(f"updated status: {tweet}")
+if approx_day is not None:
+    if not test:
+        import config
 
+        twitter = Twitter(
+            auth = OAuth(config.token, config.secret, config.consumer_key, config.consumer_secret))
+
+        tweet = f"Happy {approx_day.constant()} Approximation Day!"
+
+        results = twitter.statuses.update(status=tweet)
+    print(f"updated status: {tweet}")
